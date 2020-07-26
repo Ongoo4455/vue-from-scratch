e70769e7e0ba199c6e548d9dd9c1b4c7c9ccd127
Ongoo <erdenedavaa.storage@gmail.com>
2020-07-26 20:21:43 +0800

    Lesson 7

---
+++ src/components/Accordion.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ item_prop.title }}</h1>
+    <p @click="toggle = !toggle">
+      <strong>Details</strong>
+    </p>
+    <p v-if="toggle">{{ item_prop.description }}</p>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accordion",
+
+  props: ["item_prop"],
+
+  data() {
+    return {
+      toggle: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
